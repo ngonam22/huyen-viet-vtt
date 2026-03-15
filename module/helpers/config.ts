3fe26135b07fadbe65829c95ac7b99a9851edf61
J Ngo <ngonam22@users.noreply.github.com>
2026-03-15 21:55:02 +0700

    complete list of thiToc

--- module/helpers/config.ts
+++ module/helpers/config.ts
@@ -28,7 +28,7 @@ export const THI_TOC: ThiToc[] = [
     {
         ten: 'BOILERPLATE.ThiToc.chuot.ten',
         linhGiap: 'chuot',
-        viTri: 'noiBac',
+        viTri: 'BOILERPLATE.ThiToc.chuot.viTri',
         upgrade: [
             {
                 target: 'element',
@@ -48,6 +48,325 @@ export const THI_TOC: ThiToc[] = [
                 effects: [
                     {
                         name: 'yHoc',
+                        value: 1
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        ten: 'BOILERPLATE.ThiToc.trau.ten',
+        linhGiap: 'trau',
+        viTri: 'BOILERPLATE.ThiToc.trau.viTri',
+        upgrade: [
+            {
+                target: 'element',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'tho',
+                        value: 1
+                    }
+                ]
+            },
+            {
+                target: 'skill',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'binhPhap',
+                        value: 1
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        ten: 'BOILERPLATE.ThiToc.ho.ten',
+        linhGiap: 'ho',
+        viTri: 'BOILERPLATE.ThiToc.ho.viTri',
+        upgrade: [
+            {
+                target: 'element',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'kim',
+                        value: 1
+                    }
+                ]
+            },
+            {
+                target: 'skill',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'theThuat',
+                        value: 1
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        ten: 'BOILERPLATE.ThiToc.meo.ten',
+        linhGiap: 'meo',
+        viTri: 'BOILERPLATE.ThiToc.meo.viTri',
+        upgrade: [
+            {
+                target: 'element',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'moc',
+                        value: 1
+                    }
+                ]
+            },
+            {
+                target: 'skill',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'bieuDien',
+                        value: 1
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        ten: 'BOILERPLATE.ThiToc.ca.ten',
+        linhGiap: 'ca',
+        viTri: 'BOILERPLATE.ThiToc.ca.viTri',
+        upgrade: [
+            {
+                target: 'element',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'thuy',
+                        value: 1
+                    }
+                ]
+            },
+            {
+                target: 'skill',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'thienDinh',
+                        value: 1
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        ten: 'BOILERPLATE.ThiToc.ran.ten',
+        linhGiap: 'ran',
+        viTri: 'BOILERPLATE.ThiToc.ran.viTri',
+        upgrade: [
+            {
+                target: 'element',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'thuy',
+                        value: 1
+                    }
+                ]
+            },
+            {
+                target: 'skill',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'tamY',
+                        value: 1
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        ten: 'BOILERPLATE.ThiToc.ngua.ten',
+        linhGiap: 'ngua',
+        viTri: 'BOILERPLATE.ThiToc.ngua.viTri',
+        upgrade: [
+            {
+                target: 'element',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'hoa',
+                        value: 1
+                    }
+                ]
+            },
+            {
+                target: 'skill',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'thuongNghiep',
+                        value: 1
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        ten: 'BOILERPLATE.ThiToc.de.ten',
+        linhGiap: 'de',
+        viTri: 'BOILERPLATE.ThiToc.de.viTri',
+        upgrade: [
+            {
+                target: 'element',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'tho',
+                        value: 1
+                    }
+                ]
+            },
+            {
+                target: 'skill',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'laoDong',
+                        value: 1
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        ten: 'BOILERPLATE.ThiToc.khi.ten',
+        linhGiap: 'khi',
+        viTri: 'BOILERPLATE.ThiToc.khi.viTri',
+        upgrade: [
+            {
+                target: 'element',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'moc',
+                        value: 1
+                    }
+                ]
+            },
+            {
+                target: 'skill',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'leDao',
+                        value: 1
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        ten: 'BOILERPLATE.ThiToc.ga.ten',
+        linhGiap: 'ga',
+        viTri: 'BOILERPLATE.ThiToc.ga.viTri',
+        upgrade: [
+            {
+                target: 'element',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'kim',
+                        value: 1
+                    }
+                ]
+            },
+            {
+                target: 'skill',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'myThuat',
+                        value: 1
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        ten: 'BOILERPLATE.ThiToc.soi.ten',
+        linhGiap: 'soi',
+        viTri: 'BOILERPLATE.ThiToc.soi.viTri',
+        upgrade: [
+            {
+                target: 'element',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'tho',
+                        value: 1
+                    }
+                ]
+            },
+            {
+                target: 'skill',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'hacNghiep',
+                        value: 1
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        ten: 'BOILERPLATE.ThiToc.heo.ten',
+        linhGiap: 'heo',
+        viTri: 'BOILERPLATE.ThiToc.heo.viTri',
+        upgrade: [
+            {
+                target: 'element',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'thuy',
+                        value: 1
+                    }
+                ]
+            },
+            {
+                target: 'skill',
+                choose: 1,
+                mode: 'add',
+                effects: [
+                    {
+                        name: 'sinhTon',
                         value: 1
                     }
                 ]
